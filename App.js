b885b0af1ac0ea57bcc93c2c5045774a6ffc7fee
NeerajNuix <neeraj@nuixtech.com>
2025-09-29 12:14:28 +0530

    final test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! NeerajNuix</Text>
+      <Text>This is CI/CD Pipeline test App! </Text>
       <StatusBar style="auto" />
     </View>
   );
